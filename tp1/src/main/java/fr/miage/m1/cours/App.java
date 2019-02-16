7b83f7ab7c3eccdb356c8811546c58e2f0fc065d
traore doumbe <doumstar90@gmail.com>
2019-02-17 00:06:55 +0100

    Creation de filtre dans une classe interne anonyme

--- tp1/src/main/java/fr/miage/m1/cours/App.java
+++ tp1/src/main/java/fr/miage/m1/cours/App.java
@@ -1,54 +1,82 @@
 package fr.miage.m1.cours;
 import java.io.File;
+import java.io.FilenameFilter;
 
 public class App {
 
-    public void ListerRepertoire(File f) {
-		if(!f.isDirectory()) {
-			System.err.println();
-		}
-		else {
+// Q1:Lister le contenu d'un repertoire
+
+    public void ListerRepertoire1(File f) {
+		if(f.isDirectory()) {
 			for(String s: f.list()) {
-				System.out.println(s);
+				System.out.println("-> "+s);
 			}
+		}else{
+			System.err.println();
 		}
 	}
 	
-	public void ListerRepertoire1(File f) {
-		if(!f.exists() || !f.isDirectory()) {
-			System.err.println();
-		}
-		else {
+// Q2: Parcourir en profondeur de repertoire
+
+	public void ListerRepertoire2(File f) {
+		if(f.exists() && f.isDirectory()) {
 			for(int i=0; i<f.listFiles().length; i++) {
 				if(f.listFiles()[i].isFile()) {
-					System.out.println("\t fichier: "+f.listFiles()[i].getName());
-				}
-				else {
-					System.out.println("\t ==> répertoire: "+f.listFiles()[i].getName());
+					System.out.println("fichier: "+f.listFiles()[i].getName());
+				}else{
+					System.out.println("répertoire: "+f.listFiles()[i].getName());
 					for(int j=0; j<f.listFiles()[i].listFiles().length; j++) {
 						if(f.listFiles()[i].listFiles()[j].isFile()) {
-							System.out.println("\t \t fichier: "+f.listFiles()[i].listFiles()[j].getName());
-						}
-						else {
+							System.out.println("fichier: "+f.listFiles()[i].listFiles()[j].getName());
+						}else{
+							System.out.println("-> répertoire: "+f.listFiles()[i].listFiles()[j].getName());
 							ListerRepertoire1(f.listFiles()[i].listFiles()[j]);
 						}
 					}
 				}
 			}
+		}else{
+			System.err.println();
 		}
-    }
-     
+	}
+	
+	// Q3: Creation de filtre dans une classe interne anonyme (anonymous inner class)
 
-      
+	public void ListerRepertoire3(File f,FilenameFilter ff) {
+		if(f.exists() && f.isDirectory()) {
+			for(int i=0; i<f.listFiles(ff).length; i++) {
+				if(f.listFiles(ff)[i].isFile()) {
+					System.out.println("fichier: "+f.listFiles(ff)[i].getName());
+				}else{
+					System.out.println("répertoire: "+f.listFiles(ff)[i].getName());
+					for(int j=0; j<f.listFiles(ff)[i].listFiles(ff).length; j++) {
+						if(f.listFiles(ff)[i].listFiles(ff)[j].isFile()) {
+							System.out.println("fichier: "+f.listFiles(ff)[i].listFiles(ff)[j].getName());
+						}else{
+							System.out.println("-> répertoire: "+f.listFiles(ff)[i].listFiles(ff)[j].getName());
+							ListerRepertoire1(f.listFiles(ff)[i].listFiles(ff)[j]);
+						}
+					}
+				}
+			}
+		}else{
+			System.err.println();
+		}
+	}
+
     public static void main( String[] args ){
-
-        File f = new File("C:\\Users\\deptinfo\\Documents\\Master_Miage_S2");
-        System.out.println(f.getAbsolutePath());
-        String[] liste=f.list();
-        for (int i=0; i<liste.length; i++) {
-            File ff=new File(liste[i]);
-            if (ff.isDirectory()) System.out.println("Dossier \t"+liste[i]);
-            else System.out.println(""+ff.length()+" \t"+liste[i]);
-        }
+		App a = new App();
+		File f = new File("C:\\Users\\deptinfo\\Documents\\Master_Miage_S2");
+		FilenameFilter ff = new FilenameFilter() {
+				//apply a filter
+				@Override
+				public boolean accept(File dir, String name) {
+					boolean result;
+					return name.contains("IOT");
+				}
+			};
+		//a.ListerRepertoire1(f);
+		//a.ListerRepertoire2(f);
+		a.ListerRepertoire3(f,ff);
     }
 }
