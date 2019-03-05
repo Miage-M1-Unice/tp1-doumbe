d9342d71c885a544aab9bc087bac39a6f66848f8
traore doumbe <doumstar90@gmail.com>
2019-03-05 11:53:16 +0100

    ajout de la méthode afficheMethodes

--- tp2/src/main/java/fr/miage/m1/cours/AnalyseurDeClasse.java
+++ tp2/src/main/java/fr/miage/m1/cours/AnalyseurDeClasse.java
@@ -2,67 +2,89 @@ package fr.miage.m1.cours;
 
 import java.lang.reflect.*;
 
-public class AnalyseurDeClasse{
+public class AnalyseurDeClasse {
     Class<?> c;
-    
-    public AnalyseurDeClasse(String nomClasse) throws ClassNotFoundException{
+
+    public AnalyseurDeClasse(String nomClasse) throws ClassNotFoundException {
         c = Class.forName(nomClasse);
     }
-    
-    private void afficheEnTeteClasse() throws ClassNotFoundException{
+
+    private void afficheEnTeteClasse() throws ClassNotFoundException {
         int modifier = c.getModifiers();
-        // affiche le nom de la classe 
-        System.out.print(java.lang.reflect.Modifier.toString(modifier)+" class "+c.getName()+" ");
-        if(c.getSuperclass()!=null && c.getSuperclass()!=Object.class){
-            System.out.print("extends "+c.getSuperclass()+" "); // si classe parent exist affiche la classe
+        // affiche le nom de la classe
+        System.out.print(java.lang.reflect.Modifier.toString(modifier) + " class " + c.getName() + " ");
+        if (c.getSuperclass() != null && c.getSuperclass() != Object.class) {
+            System.out.print("extends " + c.getSuperclass() + " "); // si classe parent exist affiche la classe
         }
 
-        if(c.getInterfaces().length!=0) {
-            int testindice = c.getInterfaces().length-1;
+        if (c.getInterfaces().length != 0) {
+            int testindice = c.getInterfaces().length - 1;
             System.out.print("implements ");
-            for(int i=0; i<c.getInterfaces().length; i++) {
+            for (int i = 0; i < c.getInterfaces().length; i++) {
                 System.out.print(c.getInterfaces()[i].getName());
-                if(i<testindice) {
+                if (i < testindice) {
                     System.out.print(",");
                 }
             }
         }
         System.out.println("{");
     }
-    
-    private void afficheAttributs() throws ClassNotFoundException{
+
+    private void afficheAttributs() throws ClassNotFoundException {
         System.out.println("// Champs");
         Field[] champs = c.getDeclaredFields();
-        for(Field field: champs){
+        for (Field field : champs) {
             int modif = field.getModifiers();
-            System.out.println("  "+java.lang.reflect.Modifier.toString(modif)+" "+field.getType()+" "+field.getName()+";");
+            System.out.println("  " + java.lang.reflect.Modifier.toString(modif) + " " + field.getType() + " "
+                    + field.getName() + ";");
         }
     }
 
-    private void afficheConstructeurs() throws ClassNotFoundException{
+    private void afficheConstructeurs() throws ClassNotFoundException {
         System.out.println("\n// Constructeurs");
         Constructor<?>[] constructeurs = c.getDeclaredConstructors();
-        for(Constructor<?> cons: constructeurs) {
+        for (Constructor<?> cons : constructeurs) {
             int modif = cons.getModifiers();
-            System.out.print("  "+java.lang.reflect.Modifier.toString(modif)+" "+cons.getName()+" (");
+            System.out.print("  " + java.lang.reflect.Modifier.toString(modif) + " " + cons.getName() + " (");
             Class<?>[] parameters = cons.getParameterTypes();
-            for(int i=0; i<parameters.length; i++) {
+            for (int i = 0; i < parameters.length; i++) {
                 System.out.print(parameters[i]);
-                if(i<parameters.length-1) {
+                if (i < parameters.length - 1) {
                     System.out.print(",");
                 }
             }
-            System.out.println(");");		
+            System.out.println(");");
         }
     }
 
-    public void analyseClasse() throws ClassNotFoundException{
+    private void afficheMethodes() throws ClassNotFoundException {
+        System.out.println("\n// Methodes");
+        Method[] methodes = c.getDeclaredMethods();
+        for (Method m : methodes) {
+            int modif = m.getModifiers();
+            System.out.print("  " + java.lang.reflect.Modifier.toString(modif) + " " + m.getReturnType() + " "
+                    + m.getName() + " (");
+            Class<?>[] parametres = m.getParameterTypes();
+            int testindice = parametres.length - 1;
+            for (int i = 0; i < parametres.length; i++) {
+                System.out.print(parametres[i].getName());
+                if (i < testindice) {
+                    System.out.print(",");
+                }
+            }
+            System.out.println(");");
+        }
+        System.out.println("\n}");
+    }
+
+    public void analyseClasse() throws ClassNotFoundException {
         afficheEnTeteClasse();
         afficheAttributs();
         afficheConstructeurs();
+        afficheMethodes();
     }
 
-    public static void main(String[] args) throws ClassNotFoundException{
+    public static void main(String[] args) throws ClassNotFoundException {
         AnalyseurDeClasse ac = new AnalyseurDeClasse("java.awt.Point");
         ac.analyseClasse();
     }
